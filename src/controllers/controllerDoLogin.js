3f21dfffdd5da686ff6b0cf4a6f527287e5f3cf1
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2021-12-18 16:26:49 -0300

    camada controller do login pronta

--- src/controllers/controllerDoLogin.js
+++ src/controllers/controllerDoLogin.js
@@ -1,6 +1,16 @@
 const serviceDoLogin = require('../services/serviceDoLogin');
 
-const loginUsuario = async (req, res) => {};
+const loginUsuario = async (req, res) => {
+  const { email } = req.body;
+
+  const token = await serviceDoLogin.loginUsuario(email);
+  // Se conter uma propriedade mensage, retorna o status 400 de 'Bad request' com a mensagem que eu defino na camada services.
+  if (token.message) {
+    return res.status(400).json({ message: token.message });
+  }
+  // Retorna sucesso, com o token gerado para o usuário conseguir operar dentro do meu sistema.
+  return res.status(200).json({ token });
+};
 
 module.exports = {
   loginUsuario
