bbca97f9df0d569b4e32d975ab005c87d748b7a1
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2021-12-19 14:11:44 -0300

    criado a pagina de login com os inputs,botao e a logica

---
+++ frontend/123projetei/src/pages/Login/index.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+function Login() {
+  const [nome, setNome] = useState();
+  const [senha, setSenha] = useState();
+  const [desautorizado, setDesautorizado ] = useState(true);
+  const navegacao = useNavigate()
+
+  useEffect(() => {
+    loginIsValid();
+  }, [loginIsValid]);
+  
+    function handleNome({target}) {
+      setNome(target.value);
+    }
+
+    function handleSenha({target}) {
+      setSenha(target.value);
+    }
+
+    function handleClick() {
+      navegacao('/home');
+    }
+
+    function loginIsValid() {
+      if(nome === 'admin' && senha === 'admin') {
+        setDesautorizado(false)
+      } else {
+        setDesautorizado(true)
+      }
+    }
+
+  return (
+    <div>
+      <input type="text" onChange= { handleNome } />
+      <input type="text" onChange= { handleSenha }/>
+      <button disabled={desautorizado} onClick={ handleClick }>ENTRAR</button>
+    </div>
+  );
+
+}
+
+export default Login;
